ba6c6ec1ae6e412876692a624b7dc8cced09ce79
Nicolas Meuwly <nicomeuwly@gmail.com>
2024-02-19 21:42:00 +0100

    edit base app and add connection to db

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "WageWizard",
+  description: "Gestion des heures de travail et calcul du salaire",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
