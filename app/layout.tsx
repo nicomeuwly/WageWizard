490e6401178a87ae8f18156cafe354b7236731f7
Nicolas Meuwly <nicomeuwly@gmail.com>
2024-02-20 15:47:05 +0100

    remove manifest and service worker config due to build crash

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,50 +1,12 @@
-import type { Metadata, Viewport } from "next";
+import type { Metadata } from "next";
 import "./globals.css";
 import './globalicon.css';
 import React from "react";
 import Footer from "@/components/footer";
 
-const APP_NAME = "WageWizard";
-const APP_DEFAULT_TITLE = "WageWizard - Assistant du temps de travail et du salaire";
-const APP_TITLE_TEMPLATE = "%s - PWA App";
-const APP_DESCRIPTION = "Gestions des heures de travail et calcul du salaire.";
-
 export const metadata: Metadata = {
-  applicationName: APP_NAME,
-  title: {
-    default: APP_DEFAULT_TITLE,
-    template: APP_TITLE_TEMPLATE,
-  },
-  description: APP_DESCRIPTION,
-  appleWebApp: {
-    capable: true,
-    statusBarStyle: "default",
-    title: APP_DEFAULT_TITLE,
-  },
-  formatDetection: {
-    telephone: false,
-  },
-  openGraph: {
-    type: "website",
-    siteName: APP_NAME,
-    title: {
-      default: APP_DEFAULT_TITLE,
-      template: APP_TITLE_TEMPLATE,
-    },
-    description: APP_DESCRIPTION,
-  },
-  twitter: {
-    card: "summary",
-    title: {
-      default: APP_DEFAULT_TITLE,
-      template: APP_TITLE_TEMPLATE,
-    },
-    description: APP_DESCRIPTION,
-  },
-};
-
-export const viewport: Viewport = {
-  themeColor: "#313131",
+  title: "WageWizard",
+  description: "Gestion des heures de travail et calcul du salaire",
 };
 
 export default function RootLayout({
--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
-import {
-    PHASE_DEVELOPMENT_SERVER,
-    PHASE_PRODUCTION_BUILD,
-} from "next/constants.js";
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-export default async (phase) => {
-    let nextConfig = {};
-
-    if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-        const withSerwist = (await import("@serwist/next")).default({
-            swSrc: "app/sw.ts",
-            swDest: "public/sw.js",
-        });
-        return withSerwist(nextConfig);
-    }
-    return nextConfig;
-};
+export default nextConfig;
