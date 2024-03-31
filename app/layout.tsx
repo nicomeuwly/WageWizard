52a7f37110b0ecd6b4a711be78f35cb0758c8a13
Nicolas Meuwly <nicomeuwly@gmail.com>
2024-03-31 23:30:26 +0200

    configure pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,35 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import './globalicon.css';
 import React from "react";
 import Footer from "@/components/footer";
 
+const APP_NAME = "WageWizard";
+const APP_DEFAULT_TITLE = "WageWizard";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "Work time and wage assistant.";
+
 export const metadata: Metadata = {
-  title: "WageWizard",
-  description: "Gestion des heures de travail et calcul du salaire",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
